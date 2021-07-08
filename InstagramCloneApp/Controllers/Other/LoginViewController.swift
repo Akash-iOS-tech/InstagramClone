4baacae45f21b807c164216c723e04a66aca0937
Akash <writetome7707@gmail.com>
2021-07-08 11:17:31 -0500

    Reinstalled pods
    Created text fields for the login view

--- InstagramCloneApp/Controllers/Other/LoginViewController.swift
+++ InstagramCloneApp/Controllers/Other/LoginViewController.swift
@@ -1,10 +1,52 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    
+    private let usernameEmailField: UITextField = {
+        return UITextField()
+    }()
 
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let termsButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let privacyButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let headerView: UIView = {
+        return UIView()
+    }()
+    
+    private func addSubviews() {
+        view.addSubview(usernameEmailField)
+        view.addSubview(passwordField)
+        view.addSubview(loginButton)
+        view.addSubview(termsButton)
+        view.addSubview(privacyButton)
+        view.addSubview(headerView)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubviews()
+        view.backgroundColor = .systemBackground
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
-        view.backgroundColor = .red
+        // assign frames
     }
 }
