4ced0c616cb768b2c413d01c86ee1a8ef31ebce9
Akash <writetome7707@gmail.com>
2021-07-08 12:54:35 -0500

    Added Instagram logo to the header view Created text fields for username, password and added a button for Create Account to sign in

--- InstagramCloneApp/Controllers/Other/LoginViewController.swift
+++ InstagramCloneApp/Controllers/Other/LoginViewController.swift
@@ -2,19 +2,45 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    static let cornerRadius: CGFloat = 8.0
     
     private let usernameEmailField: UITextField = {
-        return UITextField()
+        let field = UITextField()
+        field.placeholder = "Username or Email..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        return field
     }()
-
+    
     private let passwordField: UITextField = {
         let field = UITextField()
         field.isSecureTextEntry = true
+        field.placeholder = "Password"
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = cornerRadius
+        field.backgroundColor = .secondarySystemBackground
         return field
     }()
     
     private let loginButton: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = cornerRadius
+        button.backgroundColor = .systemBlue
+        return button
     }()
     
     private let termsButton: UIButton = {
@@ -26,11 +52,18 @@ class LoginViewController: UIViewController {
     }()
     
     private let createAccount: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("New User? Create an Account", for: .normal)
+        return button
     }()
     
     private let headerView: UIView = {
-        return UIView()
+        let header = UIView()
+        header.clipsToBounds = true
+        let backgroundImageView = UIImageView(image: UIImage(named: "gradient"))
+        header.addSubview(backgroundImageView)
+        return header
     }()
     
     private func addSubviews() {
@@ -53,7 +86,29 @@ class LoginViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         // assign frames
+        headerView.frame = CGRect(x: 0, y: 0, width: view.width, height: view.height/3.0)
+        usernameEmailField.frame = CGRect(x: 25, y: headerView.bottom + 10, width: view.width - 50, height: 52)
+        passwordField.frame = CGRect(x: 25, y: usernameEmailField.bottom + 10, width: view.width - 50, height: 52)
+        loginButton.frame = CGRect(x: 25, y: passwordField.bottom + 10, width: view.width - 50, height: 52)
+        createAccount.frame = CGRect(x: 25, y: loginButton.bottom + 10, width: view.width - 50, height: 52)
+        
+        configureHeaderView()
     }
+    
+    private func configureHeaderView() {
+        guard headerView.subviews.count == 1 else { return }
+    
+    
+    guard let backgroundView = headerView.subviews.first else { return }
+        backgroundView.frame = headerView.bounds
+        
+        // Add Insta Logo
+        
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        headerView.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: headerView.width/4.0, y: view.safeAreaInsets.top, width: headerView.width/2.0, height: headerView.height - view.safeAreaInsets.top)
+}
     
     @objc private func didTapLoginButton() {
         
@@ -68,6 +123,6 @@ class LoginViewController: UIViewController {
     }
     
     @objc private func didTapCreateAccountButton() {
-         
+        
     }
 }
